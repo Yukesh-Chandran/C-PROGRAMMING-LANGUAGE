a2597f1072220594487c00c8e292a93596367e3a
YUKESH CHANDRAN M <109645230+Yukesh-Chandran@users.noreply.github.com>
2022-09-04 12:43:53 +0530

    Create printing code from 1 to 9 in words.c

---
+++ printing code from 1 to 9 in words.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a==1||a==2||a==3||a==4||a==5||a==6||a==7||a==8||a==9)
+    {
+        if(a==1)
+        printf("one");
+        else if(a==2)
+        printf("two");
+        else if(a==3)
+        printf("three");
+        else if(a==4)
+        printf("four");
+        else if(a==5)
+        printf("five");
+        else if(a==6)
+        printf("six");
+        else if(a==7)
+        printf("seven");
+        else if(a==8)
+        printf("eight");
+        else if (a==9)
+        printf("nine");    
+    }
+    else
+    printf("Greater than 9");
+}
